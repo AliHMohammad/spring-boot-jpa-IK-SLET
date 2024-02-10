c1c1b6a3a17b0c979c7eb89c20dd0d5bcf811f21
AliHMohammad <alihmohammad@hotmail.com>
2024-02-10 13:23:24 +0100

    added custom query

--- src/main/java/dk/kea/springbootdb/student/StudentRepository.java
+++ src/main/java/dk/kea/springbootdb/student/StudentRepository.java
@@ -1,14 +1,23 @@
 package dk.kea.springbootdb.student;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
 public interface StudentRepository extends JpaRepository<Student, Long> {
-
     //Vi har defineret vores egen metode (SQL-kald), som vi kan bruge i vores service-lag
+
+    //Nedenstående er implicit
     //SELECT * FROM student WHERE email = ?
     Optional<Student> findStudentByEmail(String email);
+
+    //Vi kan også være mere eksplicitte og definere præcist hvordan
+    //Querien skal se ud til metoden med @Query()
+    @Query("select s from Student s where s.name = ?1")
+    Optional<Student> findStudentByName(String name);
 }
+
+
